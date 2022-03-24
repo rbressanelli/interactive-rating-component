e13c28ecdc90c44c2365ae1b81501aa18c8e8f83
Roberto Bressanelli <rbressanelli@gmail.com>
2022-03-24 15:26:35 -0300

    refactor: ajust code and draft readme

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-const mainFunction = () => {
-  const rateButtons = document.querySelectorAll(".rating-value");
-  const submitButton = document.getElementById("submit-button");
-  const mainSelectedSection = document.querySelectorAll(".box")[0];
-  const secondSelectedSection = document.querySelectorAll(".box")[1];
-  const rateValueSpam = document.getElementById("rateValue");
-  let result = {};
+const rateButtons = document.querySelectorAll(".rating-value");
+const submitButton = document.getElementById("submit-button");
+const mainSelectedSection = document.querySelectorAll(".box")[0];
+const secondSelectedSection = document.querySelectorAll(".box")[1];
+const rateValueSpam = document.getElementById("rateValue");
+let result = {};
 
+const rateButtonsChoose = () => {
   rateButtons.forEach((button) => {
     button.addEventListener("click", function (e) {
       rateButtons.forEach((but) => {
@@ -18,15 +18,25 @@ const mainFunction = () => {
       selectedButton.classList.add("selected");
     });
   });
+};
 
-  const rated = (e) => {
-    if (!result.value) {
-      return
-    }
-    mainSelectedSection.classList.add("hidden");
-    secondSelectedSection.classList.remove("hidden");
-    rateValueSpam.innerText = `${result.value}`;
-  };
+const rated = (e) => {
+  if (!result.value) {
+    rateButtons.forEach((value) => {
+      value.classList.add("blinking");
+      setInterval(() => {
+        value.classList.remove("blinking");
+      }, 200);
+    });
+    return;
+  }
+  mainSelectedSection.classList.add("hidden");
+  secondSelectedSection.classList.remove("hidden");
+  rateValueSpam.innerText = `${result.value}`;
+};
+
+const mainFunction = () => {
+  rateButtonsChoose();
   submitButton.addEventListener("click", rated);
 };
 
